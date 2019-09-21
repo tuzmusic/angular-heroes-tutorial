4b41cd6637dbb0619bac16b2e8d616d920f3dcac
Jonathan Tuzman <tuzmusic@gmail.com>
2019-09-21 18:37:28 -0400

    complete pt3

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from "@angular/core";
+import { Hero } from "../hero";
+
+@Component({
+  selector: "app-hero-detail",
+  templateUrl: "./hero-detail.component.html",
+  styleUrls: ["./hero-detail.component.css"]
+})
+export class HeroDetailComponent implements OnInit {
+  // allow hero property to be passed in
+  // (i.e., declaring "props")
+  @Input() hero: Hero;
+
+  constructor() {}
+
+  ngOnInit() {}
+}
