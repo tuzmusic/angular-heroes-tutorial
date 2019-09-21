7b7766e4fe97c3f1e64b7704117abc75340b7419
Jonathan Tuzman <tuzmusic@gmail.com>
2019-09-21 18:24:58 -0400

    first commit of new local repo

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from "@angular/core";
+import { Hero } from "../hero";
+import { HEROES } from "../mock-heroes";
+
+@Component({
+  selector: "app-heroes",
+  templateUrl: "./heroes.component.html",
+  styleUrls: ["./heroes.component.css"]
+})
+export class HeroesComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+
+  heroes = HEROES;
+
+  selectedHero: Hero;
+
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
+  }
+}
